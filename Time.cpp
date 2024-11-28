6ad9521e57c2e67ea4723cea5b30594a694b1161
Yuri Benditovich <yuri.benditovich@daynix.com>
2024-11-29 00:24:05 +0200

    move time handler to use commands
    Signed-off-by: Yuri Benditovich <yuri.benditovich@daynix.com>

--- Time.cpp
+++ Time.cpp
@@ -1,54 +1,56 @@
 #include "stdafx.h"
 
+class tBeginTime
+{
+public:
+    tBeginTime(ULONG Resolution) : m_Resolution(Resolution)
+    {
+        m_Error = timeBeginPeriod(m_Resolution);
+    }
+    ~tBeginTime()
+    {
+        if (!m_Error)
+            timeEndPeriod(m_Resolution);
+    }
+    ULONG Error() const { return m_Error; }
+private:
+    ULONG m_Resolution;
+    ULONG m_Error = 0;
+};
+
 class CTimeHandler : public CCommandHandler
 {
 public:
-    CTimeHandler() : CCommandHandler("time", "time related operations", 1) {}
-private:
-    void ReportPerfCounter();
-    void Measure(const CStringArray& Parameters);
-    int Run(const CStringArray& Parameters) override
+    CTimeHandler() : CCommandHandler("time", "time related operations", 1)
     {
-        if (!Parameters[0].CompareNoCase("perf")) {
-            ReportPerfCounter();
-        }
-        else if (!Parameters[0].CompareNoCase("mm")) {
-            UINT value = atoi(Parameters[1]);
-            LOG("starting timer period for resolution of %d ms", value);
-            ULONG err = timeBeginPeriod(value);
-            if ( err == TIMERR_NOERROR) {
-                puts("Hit ENTER to end the timer period");
-                getchar();
-                timeEndPeriod(value);
-            } else {
-                LOG("failed, error %d", err);
-                TIMECAPS caps;
-                if (timeGetDevCaps(&caps, sizeof(caps)) == TIMERR_NOERROR) {
-                    LOG("possible values %d ... %d", caps.wPeriodMin, caps.wPeriodMax);
-                }
-            }
-        } else {
-            Measure(Parameters);
-        }
-        return 0;
+        DeclareCommand("perf", ReportPerfCounter, 0, 0);
+        DeclareCommand("wait", DoWait, 1, fMoreOK);
+        DeclareCommand("mm", BeginEnd, 1, 0);
+        DeclareCommand("anything", Measure, 1, fMatchesAll | fMoreOK);
     }
+private:
+    static int ReportPerfCounter(const CStringArray& Parameters);
+    static int DoWait(const CStringArray& Parameters);
+    static int Measure(const CStringArray& Parameters);
+    static int BeginEnd(const CStringArray& Parameters);
     void Help(CStringArray& a) override
     {
         a.Add("\"<command line to run>\"\tRun command line and report execution time");
         a.Add("perf\t\t\tReport performance counter resolution");
-        a.Add("mm <millies>\t\tStart multimedia timer");
+        a.Add("mm <millies>\t\tStart multimedia timer period or 0 for query");
+        a.Add("wait <millies> [resolution=1]\t\twait with increased resolution, 0 for default");
     }
 };
 
-void CTimeHandler::ReportPerfCounter()
+int CTimeHandler::ReportPerfCounter(const CStringArray& Parameters)
 {
     LARGE_INTEGER li;
     QueryPerformanceFrequency(&li);
     LOG("Perf counter frequency %I64d", li.QuadPart);
-
+    return 0;
 }
 
-void CTimeHandler::Measure(const CStringArray& Parameters)
+int CTimeHandler::Measure(const CStringArray& Parameters)
 {
     CString s = Parameters[0];
     for (UINT i = 1; i < Parameters.GetCount(); ++i) {
@@ -61,6 +63,45 @@ void CTimeHandler::Measure(const CStringArray& Parameters)
     int res = system(s);
     ULONG t2 = GetTickCount();
     LOG("Done(%d) in %d ms", res, t2 - t1);
+    return res;
 }
+
+int CTimeHandler::BeginEnd(const CStringArray& Parameters)
+{
+    UINT value = atoi(Parameters[0]);
+    LOG("starting timer period for resolution of %d ms", value);
+    tBeginTime begin(value);
+    if (!begin.Error()) {
+        puts("Hit ENTER to end the timer period");
+        getchar();
+    } else {
+        LOG("failed, error %d", begin.Error());
+        TIMECAPS caps;
+        if (timeGetDevCaps(&caps, sizeof(caps)) == TIMERR_NOERROR) {
+            LOG("possible values %d ... %d", caps.wPeriodMin, caps.wPeriodMax);
+        }
+    }
+
+    return 0;
+}
+
+int CTimeHandler::DoWait(const CStringArray& Parameters)
+{
+    LARGE_INTEGER li, pt1, pt2;
+    QueryPerformanceFrequency(&li);
+    UINT value = atoi(Parameters[0]);
+    UINT resolution = 1;
+    if (Parameters.GetCount() > 1) {
+        resolution = atoi(Parameters[1]);
+    }
+    LOG("sleeping for %d ms", value);
+    tBeginTime begin(resolution);
+    QueryPerformanceCounter(&pt1);
+    Sleep(value);
+    QueryPerformanceCounter(&pt2);
+    LOG("done in %d ms", (ULONG)(((pt2.QuadPart - pt1.QuadPart) * 1000) / li.QuadPart));
+    return 0;
+}
+
 
 static CTimeHandler timeh;
