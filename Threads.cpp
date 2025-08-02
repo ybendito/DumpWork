bd3bbd22f0c33177d125696f10ea6af79503e33e
Yuri Benditovich <yuri.benditovich@daynix.com>
2025-08-03 02:36:58 +0300

    extend 'load' command with memory mapped file load
    Signed-off-by: Yuri Benditovich <yuri.benditovich@daynix.com>

--- Threads.cpp
+++ Threads.cpp
@@ -1,24 +1,70 @@
 #include "stdafx.h"
-#include <NTSecAPI.h>
+
+#define MEM_SIZE_MB     1024
+
+thread_local LONGLONG gThreadPokes = 0;
 
 class CLoadingThread : public CThreadOwner
 {
 public:
-    CLoadingThread(CWaitableObject& WaitOn, ULONG& Loops) :
-        m_Obj(WaitOn),
+    CLoadingThread(CWaitableObject& WaitOn) :
+        m_Obj(WaitOn)
+    {
+
+    }
+protected:
+    void ThreadProc() override
+    {
+        m_Obj.Wait();
+    }
+private:
+    CWaitableObject& m_Obj;
+    void ThreadTerminated(tThreadState previous) override
+    {
+        //LOG("Thread finishes (%lld)", gThreadPokes);
+        m_Obj.Signal();
+        __super::ThreadTerminated(previous);
+    }
+};
+
+class CLoadingThreadMemory : public CLoadingThread
+{
+public:
+    CLoadingThreadMemory(CWaitableObject& WaitOn) : CLoadingThread(WaitOn)
+    {
+
+    }
+private:
+    void ThreadProc() override
+    {
+        __super::ThreadProc();
+        while (ShouldContinueRunning()) {
+            CMemoryMappedFile f(MEM_SIZE_MB);
+            auto n = f.Poke();
+            //LOG("+%d", n);
+            gThreadPokes += n;
+        }
+    }
+};
+
+
+class CLoadingThreadCpu : public CLoadingThread
+{
+public:
+    CLoadingThreadCpu(CWaitableObject& WaitOn, ULONG& Loops) :
+        CLoadingThread(WaitOn),
         m_Loops(Loops)
     {
 
     }
 private:
-    CWaitableObject& m_Obj;
     ULONG& m_Loops;
     void ThreadProc() override
     {
+        __super::ThreadProc();
         BYTE hash[128] = {};
         BYTE data[sizeof(hash) * 8] = {};
         RtlGenRandom(data, sizeof(data));
-        m_Obj.Wait();
         while (ShouldContinueRunning()) {
             for (ULONG i = 0; i < m_Loops; ++i) {
                 HashData(data, sizeof(data), hash, sizeof(hash));
@@ -26,23 +72,15 @@ private:
             Sleep(1);
         }
     }
-    void ThreadTerminated(tThreadState previous) override
-    {
-        m_Obj.Signal();
-        __super::ThreadTerminated(previous);
-    }
 };
 
 class CThreadCollection : public CThreadOwner
 {
     const ULONG MAX_THREADS = 4096;
 public:
-    CThreadCollection(ULONG ThreadsCount) :
-        m_Semaphore(MAX_THREADS, MAX_THREADS)
+    CThreadCollection() : m_Semaphore(0, MAX_THREADS)
     {
-        for (UINT i = 0; i < ThreadsCount; ++i) {
-            AddThread();
-        }
+
     }
     ~CThreadCollection()
     {
@@ -55,6 +93,7 @@ public:
         for (INT i = 0; i < m_Threads.GetCount(); ++i) {
             m_Semaphore.Wait();
         }
+        //LOG("All threads finished");
         for (INT i = 0; i < m_Threads.GetCount(); ++i) {
             CLoadingThread* p = m_Threads[i];
             delete p;
@@ -73,14 +112,19 @@ public:
         }
     }
     ULONG Load() const { return m_Loops; }
-    void AddThread()
+    void AddThread(bool MemoryStress)
     {
-        CLoadingThread* p = new CLoadingThread(m_Semaphore, m_Loops);
+        CLoadingThread* p;
+        if (MemoryStress) {
+            p = new CLoadingThreadMemory(m_Semaphore);
+        } else {
+            p = new CLoadingThreadCpu(m_Semaphore, m_Loops);
+        }
         if (p) {
             m_Threads.Add(p);
             p->StartThread();
+            m_Semaphore.Signal();
         }
-        m_Semaphore.Signal();
     }
 private:
     void ThreadProc() override
@@ -94,16 +138,32 @@ private:
     ULONG m_Loops = 1;
 };
 
-static int CreateThreads(const char *Param)
+static int CreateThreads(const char *Activity, const char *Param)
 {
     ULONG numThreads = atoi(Param);
+    bool bCpu = !_stricmp(Activity, "cpu");
+    bool bMem = !_stricmp(Activity, "mem");
+    if (!bCpu && !bMem) {
+        LOG("Unknown type of activity");
+        return 1;
+    } else if (bMem) {
+        LOG("Using %d MB per thread", MEM_SIZE_MB);
+    }
 
-    CThreadCollection t(numThreads);
+    CThreadCollection t;
+
+    for (ULONG i = 0; i < numThreads; ++i) {
+        t.AddThread(bMem);
+    }
 
     char line[256] = "";
     char* s = line;
-    puts("Interactive mode:");
-    puts("i(nc),d(ec),t(read add),b(usy loop toggle)");
+    if (!bMem) {
+        puts("Interactive mode: q to exit");
+        puts("i(nc),d(ec),t(read add),b(usy loop toggle)");
+    } else {
+        puts("Interactive mode: 'q' to exit, 't' to add thread");
+    }
     do
     {
         s = fgets(line, sizeof(line), stdin);
@@ -117,7 +177,7 @@ static int CreateThreads(const char *Param)
                 LOG("current loop limit 0x%X", t.Load());
                 break;
             case 't':
-                while (s[n++] == s[0]) t.AddThread();
+                while (s[n++] == s[0]) t.AddThread(bMem);
                 break;
             case 'b':
                 t.ToggleBusyLoop();
@@ -139,15 +199,16 @@ static int CreateThreads(const char *Param)
 class CThreadsHandler : public CCommandHandler
 {
 public:
-    CThreadsHandler() : CCommandHandler("load", "Load CPU by threads", 1) {}
+    CThreadsHandler() : CCommandHandler("load", "Load CPU by threads", 2) {}
 private:
     int Run(const CStringArray& Parameters) override
     {
-        return CreateThreads(Parameters[0]);
+        return CreateThreads(Parameters[0], Parameters[1]);
     }
     void Help(CStringArray& a) override
     {
-        a.Add("<number of threads>");
+        a.Add("cpu <number of threads>");
+        a.Add("mem <number of threads>");
     }
 };
 
